f0539b2670b392bf7243bbe3c7072a960f23b0c3
misrori <ormraat.pte@gmail.com>
2017-04-05 14:00:28 +0200

    add_plotly

--- create_dashboard.R
+++ create_dashboard.R
@@ -1,41 +1,36 @@
 source('downloading_data.R')
-install.packages("install.load")
+#install.packages("install.load")
 library(install.load)
 load_package("DT","data.table","stringr", "plotly")
 list_of_markets <- c("TSLA", "NVDA", "AMD", "AAPL", "GE", "MU", "OLED")
 adatom <- prices(list_of_markets)#month-1, day, year
 adatom$Date <- as.Date(adatom$Date)
 
-my_days <- sort(unique(adatom$Date), decreasing = T)[c(1:90)]
+my_days <- sort(unique(adatom$Date), decreasing = T)[c(1:20)]
 adatom <- data.table(adatom[adatom$Date %in% my_days,])
 setorder(adatom, ticker, Date)
-adat <- adatom[, valtozas:=(Close/shift(Close, 1L, type = "lag")-1)*100, by=ticker ]
 
 
 
+my_df <- data.table()
 
+for(i in list_of_markets){
+  tmp_data <-adatom[ticker==i,]
+  current <- tmp_data$Close[1]
+  valtozasok <- c(0, (( tmp_data$Close[2:20]/current)-1)*100)
+  #napok <-c('Today','1day before', '3 day before', '4 day before', '5 day before','10 day before','20 day before', '30 day before') 
+  tmp_data$valtozasok <- valtozasok
+  #tmp_data$my_days <- napok
+  my_df <- rbind(my_df, tmp_data)
 
+}
 
-f <- list(
-  family = "Courier New, monospace",
-  size = 18,
-  color = "#7f7f7f"
-)
-x <- list(
-  title = "Dátum",
-  titlefont = f
-)
-y <- list(
-  title = "Összeg",
-  titlefont = f
-)
-
-a <- data.table(data_to_plot())
-ab <- a[,.('osszeg'= sum(Ertek_KP_Millio)) , by=.(my_Date, Státusz)] 
-setorder(ab, my_Date)
-abc <- ab[, .(my_Date, osszegem = cumsum(osszeg) ), by= .(Státusz) ]
-
-p<-plot_ly(adat, x = ~Date, y = ~Close, color =~ticker)%>%
-  add_lines() #%>%layout(autosize = F, width = 1000, height = 800, margin = m, xaxis = x, yaxis = y )
+p<-plot_ly(my_df, x = ~Date, y = ~valtozasok, color =~ticker, text= ~Close)%>%
+  add_lines()
 
 p
+
+
+
+
+
