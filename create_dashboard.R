144125e0a274756312f881049e25b1826a4041df
misrori <ormraat.pte@gmail.com>
2017-04-05 09:43:24 +0200

    list

--- create_dashboard.R
+++ create_dashboard.R
@@ -2,7 +2,7 @@ source('downloading_data.R')
 install.packages("install.load")
 library(install.load)
 load_package("DT","data.table","stringr")
-list_of_markets <- c("TSLA", "NVDA", "AMD", "AAPL", "GE", "MU")
+list_of_markets <- c("TSLA", "NVDA", "AMD", "AAPL", "GE", "MU", "OLED")
 adatom <- prices(list_of_markets)#month-1, day, year
 
 
